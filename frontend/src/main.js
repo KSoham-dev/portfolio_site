7cf7b6b25a744ad8cce6f69249923ee58ddfafa4
KSoham-dev <sohamkulkarni709@gmail.com>
2026-01-05 18:58:20 +0000

    added vercel analytics

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import 'remixicon/fonts/remixicon.css'
+import { inject } from '@vercel/analytics'
+
+inject()
 
 createApp(App).mount('#app')
